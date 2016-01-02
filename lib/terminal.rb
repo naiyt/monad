894311f8b04cd0b09aa5ecce86e7031429d4370a
Nate Collings <nate@natecollings.com>
2016-01-02 13:32:04 -0700

    Command setup

--- lib/terminal.rb
+++ lib/terminal.rb
@@ -12,10 +12,27 @@ module Monad
     end
 
     def update
+      if @current_command
+        run_command(@current_command)
+        @current_command = nil
+      end
     end
 
     def draw
       @command_line.draw(Monad::ZOrder::UI)
+    end
+
+    def button_down(id)
+      if id == Gosu::KbReturn
+        @current_command = @window.text_input.text
+        @window.text_input.text = ''
+      end
+    end
+
+    private
+
+    def run_command(command)
+      puts "Running command: `#{command}`"
     end
   end
 end
