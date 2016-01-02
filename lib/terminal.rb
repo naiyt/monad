850468bb233f92d35dec5c33ecd74bf2a8b919c6
Nate Collings <nate@natecollings.com>
2016-01-02 14:25:09 -0700

    Render commands and output

--- lib/terminal.rb
+++ lib/terminal.rb
@@ -1,6 +1,6 @@
 module Monad
   class Terminal < GosuRPG::GameState
-    PROMPT_PADDING = 10
+    PROMPT_PADDING = 0
 
     def initialize(shell)
       super()
@@ -10,20 +10,30 @@ module Monad
 
       @shell = shell
       @window.caption = 'Monad'
+
       @command_line = GosuRPG::TextField.new(@window, @font,
         PROMPT_PADDING, @window.height - (@font.height + PROMPT_PADDING))
       @window.text_input = @command_line
+
+      @to_display = []
     end
 
     def update
       if @current_command
         res, cmd = @shell.handle_commands(@current_command)
-        @window.text_input.text = res
+        @to_display << @current_command
+        @to_display << res
         @current_command = nil
       end
     end
 
     def draw
+      @to_display.reverse.each_with_index do |text, index|
+        x = PROMPT_PADDING
+        y = @window.height - (@command_line.height * (index + 2))
+        @font.draw(text, x, y, Monad::ZOrder::UI)
+      end
+
       @command_line.draw(Monad::ZOrder::UI)
     end
 
