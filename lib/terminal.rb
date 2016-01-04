921a292aa52958a532fc5ef11ba334ad86f0e609
Nate Collings <nate@natecollings.com>
2016-01-03 17:47:56 -0700

    Update result buffering to work with stderr and stdout

--- lib/terminal.rb
+++ lib/terminal.rb
@@ -21,12 +21,11 @@ module Monad
     end
 
     def update
-      if @current_command
-        add_to_buffer(@current_command)
-        res, cmd = @shell.handle_commands(@current_command)
-        res = '' unless res
-        res.split("\n").each { |partial| add_to_buffer(partial) }
-        @current_command = nil
+      if @current_input
+        add_to_buffer(@current_input)
+        res, cmd = @shell.handle_input(@current_input)
+        add_res_to_buffer(res)
+        @current_input = nil
         @current_script.handle_command(res, cmd)
       end
     end
@@ -43,13 +42,25 @@ module Monad
 
     def button_down(id)
       if id == Gosu::KbReturn
-        @current_command = @window.text_input.text
+        @current_input = @window.text_input.text
         @window.text_input.text = ''
       end
     end
 
     def add_to_buffer(text)
       @text_buffer << text
+    end
+
+    private
+
+    def add_res_to_buffer(res)
+      # TODO: colorize stderr
+      res = if res.has_key?(:stderr)
+              res[:stderr] || ''
+            elsif res.has_key?(:stdout)
+              res[:stdout] || ''
+            end
+      res.split("\n").each { |partial| add_to_buffer(partial) }
     end
   end
 end
