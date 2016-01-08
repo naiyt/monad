9dd3c631eb5e9b46bdc3e8c6fd7b836f32f92e71
Nate Collings <nate@natecollings.com>
2016-01-07 19:53:08 -0700

    Revert "lib -> game"
    This reverts commit 85a1004780add6da59afdc6ee9767158b4947ad1.

--- main.rb
+++ main.rb
@@ -1,7 +1,7 @@
-require_relative 'game/gosu_stuff/gosu_stuff'
-require_relative 'game/config/config'
-Dir["./game/game_states/*.rb"].each { |f| require f }
-Dir["./game/scripts/*.rb"].each { |f| require f }
+require_relative 'lib/gosu_stuff/gosu_stuff'
+require_relative 'lib/config/config'
+Dir["./lib/game_states/*.rb"].each { |f| require f }
+Dir["./lib/scripts/*.rb"].each { |f| require f }
 
 shell    = ShellSim::Shell.new('root', 'password')
 terminal = Monad::GameStates::Terminal.new(shell)
