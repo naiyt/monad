49430fdc3c4fb04fe0a417f573e8db6b6662e23f
Nate Collings <nate@natecollings.com>
2016-01-10 14:30:45 -0700

    Line breaks on command history

--- lib/game_states/terminal.rb
+++ lib/game_states/terminal.rb
@@ -37,14 +37,8 @@ module Monad
       end
 
       def draw
-        @text_buffer.reverse.each_with_index do |text, index|
-          x = PROMPT_PADDING
-          y = @window.height - (@command_line.height * (index + 2))
-          @font.draw(text, x, y, Monad::ZOrder::UI)
-        end
-
+        draw_text_buffer!
         @command_line.draw(Monad::ZOrder::UI)
-
         @font.draw(@prompt_text, PROMPT_PADDING, cli_y_pos, Monad::ZOrder::UI)
       end
 
@@ -61,6 +55,30 @@ module Monad
       end
 
       private
+
+      def draw_text_buffer!
+        index_offset = 2 # 1 for command prompt, one for zero indexing
+
+        @text_buffer.reverse.each_with_index do |text, index|
+          if @font.text_width(text) > @window.width
+            first_half = text[0, allowed_chars]
+            second_half = text[allowed_chars..-1]
+            @font.draw(second_half, PROMPT_PADDING, y_text_pos(index + index_offset), Monad::ZOrder::UI)
+            index_offset += 1
+            @font.draw(first_half, PROMPT_PADDING, y_text_pos(index + index_offset), Monad::ZOrder::UI)
+          else
+            @font.draw(text, PROMPT_PADDING, y_text_pos(index + index_offset), Monad::ZOrder::UI)
+          end
+        end
+      end
+
+      def y_text_pos(offset)
+        @window.height - (@command_line.height * (offset))
+      end
+
+      def allowed_chars
+        Integer(@window.width / @font.text_width(' '))
+      end
 
       def add_res_to_buffer(res)
         # TODO: colorize stderr
